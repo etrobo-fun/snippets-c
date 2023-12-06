9382c1b73bd891dbc0eeb61c91b56dd5c9bec870
azurata <account@azurata.me>
2023-12-06 13:39:01 +0900

    Create moving_average_filter.c

---
+++ moving_average_filter.c
@@ -0,0 +1,31 @@
+#define SIZE 3
+
+static int inputs[SIZE];
+
+void init_average_filter(void)
+{
+  for(int i = 0; i < SIZE)
+  {
+    inputs[i] = 0;
+  }
+}
+
+int average_filter(int input)
+{
+  // 更新
+  for(int i = 0; i < SIZE - 1)
+  {
+    inputs[i + 1] = inputs[i];
+  }
+  inputs[0] = input;
+
+  int sum = 0;
+
+  // 直近の総和を計算
+  for(int i = 0; i < SIZE; i++)
+  {
+    sum += inputs[i];
+  }
+
+  return sum / SIZE;
+}
