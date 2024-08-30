0dde00fc7f23d7566012b15614b89de93c376467
azurata <git@azurata.me>
2024-08-31 00:50:56 +0900

    fix: ゼロ除算に対処

--- src/rgb2hsv.cpp
+++ src/rgb2hsv.cpp
@@ -1,4 +1,5 @@
 #include "rgb2hsv.h"
+#include <stdio.h>
 
 void convert_rgb_to_hsv(rgb *in, hsv *out)
 {
@@ -12,10 +13,14 @@ void convert_rgb_to_hsv(rgb *in, hsv *out)
   uint16_t diff_max_min = max - min;
 
   uint16_t v = max;
-  uint16_t s = 255 * diff_max_min / max;
+  uint16_t s = 0 < max ? 255 * diff_max_min / max : 0;
 
   int16_t h_;
-  if(max == r)
+  if(diff_max_min == 0)
+  {
+    h_ = 0;
+  }
+  else if(max == r)
   {
     h_ = 60 * (g - b) / diff_max_min;
   }
